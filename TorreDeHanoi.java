e2f78e184d8a89f2524d8c0f596635b112b334ba
Matheus Ribamar <matheus2007file@hotmail.com>
2022-04-13 19:46:26 -0300

    First Commit

---
+++ TorreDeHanoi.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class TorreDeHanoi {
+	public static void main(String[] args) {
+		// Declaração de variáveis
+		int numero = 0;
+
+		// Entrada de dados
+		numero = lerNumeroDoTeclado();
+
+		// Processamento
+		movimentarTorreDeHanoi(numero, "Torre1", "Torre2", "Torre3");
+	}
+
+	private static int lerNumeroDoTeclado() {
+		int retorno = 0;
+
+		Scanner leitor = new Scanner(System.in);
+
+		System.out.println("Digite um numero: ");
+		retorno = leitor.nextInt();
+		leitor.close();
+		return retorno;
+	}
+
+	private static void movimentarTorreDeHanoi(int numero, String origem, String meio, String destino) {
+		if (numero > 0) {
+			movimentarTorreDeHanoi(numero - 1, origem, destino, meio);
+			System.out.println("Movimentar: " + numero + " da " + origem + " para o " + destino);
+			movimentarTorreDeHanoi(numero - 1, meio, origem, destino);
+		}
+	}
+
+}
